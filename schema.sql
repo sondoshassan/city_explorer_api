38bd527703cddedb727845f27175def232e21917
sondoshassan <sondos199628@gmail.com>
2020-04-07 22:42:53 +0300

    fix the table in SQL

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS location;
 
 CREATE TABLE IF NOT EXISTS location(
+id SERIAL PRIMARY KEY, 
 city VARCHAR(255),
 formatted_query VARCHAR(255),
-latitude VARCHAR(255),
-longitude VARCHAR(255)
+latitude NUMERIC,
+longitude NUMERIC
 );
 
