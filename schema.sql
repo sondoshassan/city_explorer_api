60998898552c13a02a7ae2e00f9a36d43ba84e82
sondoshassan <sondos199628@gmail.com>
2020-04-08 17:59:05 +0300

    finish movie

--- schema.sql
+++ schema.sql
@@ -8,3 +8,17 @@ latitude NUMERIC,
 longitude NUMERIC
 );
 
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE IF NOT EXISTS movies(
+id SERIAL PRIMARY KEY, 
+city VARCHAR(255),
+title VARCHAR(255),
+overview VARCHAR(600),
+average_votes NUMERIC,
+total_votes NUMERIC,
+image_url VARCHAR(600),
+popularity NUMERIC,
+released_on VARCHAR(255)
+);
+
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ let server = express();
 const PORT = process.env.PORT || 3000;
 server.use(cors());
 const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error',error => console.log(error));
+client.on('error', error => console.log(error));
 let long;
 let lat;
 
 // for the location
-server.get('/location',convertSearchQuery);
+server.get('/location', convertSearchQuery);
 
 function convertSearchQuery(req, res) {
   const nameCity = req.query.city;
@@ -23,23 +23,26 @@ function convertSearchQuery(req, res) {
 }
 
 function chechDataBase(nameCity) {
-  let SQL = 'SELECT * FROM location WHERE city = ($1)';
+  let SQL = 'SELECT * FROM location WHERE city = ($1);';
   let safeValues = [nameCity];
   return client.query(SQL, safeValues)
     .then(val => {
       if (val.rows[0]) {
         console.log('from tab ', val.rows[0]);
+        long = val.rows[0].longitude;
+        lat = val.rows[0].latitude;
         return val.rows[0];
       }
       else {
         console.log('are you enter');
         return locationData(nameCity)
           .then(val => {
-            console.log('ssssssssss ',val);
-            return val;});
+            console.log('ssssssssss ', val);
+            return val;
+          });
       }
     })
-    .catch(err => console.log(err));
+    .catch(err => errorHandler(err));
 }
 
 function locationData(nameCity) {
@@ -52,15 +55,15 @@ function locationData(nameCity) {
       long = locate.longitude;
       lat = locate.latitude;
       let SQL = 'INSERT INTO location (city,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4);';
-      let safeValues = [locate.city,locate.formatted_query,locate.latitude,locate.longitude];
-      client.query(SQL,safeValues)
-        .then(result => {return result;});
+      let safeValues = [locate.city, locate.formatted_query, locate.latitude, locate.longitude];
+      client.query(SQL, safeValues)
+        .then(result => { return result; });
       return locate;
     });
 
 }
 
-function Location(nameCity,data){
+function Location(nameCity, data) {
   this.city = nameCity;
   this.formatted_query = data[0].display_name;
   this.latitude = data[0].lat;
@@ -68,9 +71,9 @@ function Location(nameCity,data){
 }
 
 // localhost:3030/weather
-server.get('/weather',responseWeather);
+server.get('/weather', responseWeather);
 
-function responseWeather(req,res){
+function responseWeather(req, res) {
   const weatherCity = req.query.city;
   dataWeather(weatherCity)
     .then(val => res.status(200).json(val));
@@ -80,26 +83,27 @@ function dataWeather(weatherCity) {
   const key = process.env.WEATHER_KEY_API;
   const dataWeather = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weatherCity}&key=${key}`;
   return superagent.get(dataWeather)
-    .then(val =>{
+    .then(val => {
       const dataArray = val.body.data;
       let array = dataArray.map(val => {
         const weather = new Weather(val);
-        return weather;});
+        return weather;
+      });
       return array;
     })
 }
 
-function Weather(dataWeath){
+function Weather(dataWeath) {
   this.forecast = dataWeath.weather.description;
   this.time = dataWeath.valid_date;
 }
 
 // localhost:3030/trails?city=amman
-server.get('/trails',trailHandler);
+server.get('/trails', trailHandler);
 
-function trailHandler(req,res){
-  let nameCity = req.query.city;
-  dataTrails(nameCity)
+function trailHandler(req, res) {
+  // let nameCity = req.query.city;
+  dataTrails()
     .then(val => res.status(200).json(val));
 }
 
@@ -118,11 +122,11 @@ function dataTrails() {
     });
 }
 
-function Campgrounds(data){
-  this.name= data.name;
+function Campgrounds(data) {
+  this.name = data.name;
   this.location = data.location;
-  this.length= data.length;
-  this.stars=data.stars;
+  this.length = data.length;
+  this.stars = data.stars;
   this.star_votes = data.starVotes;
   this.summary = data.summary;
   this.trail_url = data.url;
@@ -131,28 +135,127 @@ function Campgrounds(data){
   this.condition_time = data.conditionDate.split(' ')[1];
 
 }
+// MOVIE
 
-server.get('/',(req,res)=>{
+server.get('/movies', movieHandler);
+
+function movieHandler(req, res) {
+  const nameCity = req.query.city;
+  chechDataBaseMovie(nameCity)
+    .then(val => res.status(200).send(val));
+}
+
+function chechDataBaseMovie(nameCity) {
+  let SQL = 'SELECT * FROM movies WHERE city = ($1);';
+  let safeValues = [nameCity];
+  return client.query(SQL, safeValues)
+    .then(val => {
+      if (val.rows[0]) {
+        console.log('from tabMovie SQL');
+        return val.rows.slice(0,19);
+      }
+      else {
+        console.log('are you enterMovie from API');
+        return dataMovie(nameCity)
+          .then(val => {
+            return val;
+          });
+      }
+    })
+    .catch(err => errorHandler(err));
+}
+
+
+function dataMovie(nameCity) {
+  const key = process.env.MOVIE_API;
+  const secondUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&city=${nameCity}`;
+  return superagent.get((secondUrl))
+    .then(val => {
+      const dataArray = val.body.results;
+      let arrayResult = dataArray.map(val => {
+        const theTarget = new Movies(val);
+        let SQL = 'INSERT INTO movies (city,title,overview,average_votes,total_votes,image_url,popularity,released_on) VALUES ($1,$2,$3,$4,$5,$6,$7,$8);';
+        let safeValues = [nameCity,theTarget.title,theTarget.overview,theTarget.average_votes,theTarget.total_votes,theTarget.image_url,theTarget.popularity,theTarget.released_on];
+        client.query(SQL, safeValues)
+          .then(result => {
+            return result; })
+          .catch(err => console.log(err));
+        return theTarget;
+      });
+      return arrayResult;
+    })
+    .catch(err => errorHandler(err));
+}
+
+function Movies(data) {
+  this.title = data.title;
+  this.overview = data.overview;
+  this.average_votes = data.vote_average;
+  this.total_votes = data.vote_count;
+  this.image_url = data.poster_path;
+  this.popularity = data.popularity;
+  this.released_on = data.release_date;
+}
+// yelp
+
+
+server.get('/movies', yelpHandler);
+function yelpHandler(req, res) {
+  dataYelp()
+    .then(val => res.status(200).json(val));
+}
+
+
+function dataYelp() {
+  const apiKey = process.env.YELP_API;
+  let url = 'https://api.yelp.com/v3/businesses/search';
+  return superagent.get(url)
+    .set('Authorization',`Bearer ${apiKey}`)
+    .then(val => {
+      const dataArray = val.body.businesses;
+      let arrayResult = dataArray.map(val => {
+        const theTarget = new Yelp(val);
+        return theTarget;
+      });
+      return arrayResult;
+    });
+}
+
+function Yelp(data) {
+  this.name = data.name;
+  this.image_url = data.image_url;
+  this.price = data.price;
+  this.rating = data.rating;
+  this.url = data.url;
+}
+
+
+
+//
+
+server.get('/', (req, res) => {
   res.send('Hi! open city explorer and enjoye :)');
 })
-server.get('*',(req,res)=>{
+server.get('*', (req, res) => {
   res.status(404).send('The page is not excit');
-})
-server.use((error,req,res)=>{
-  res.status(500).send(new UserError(error));
 });
 
-function UserError(error){
+function errorHandler(error, request, response) {
+  response.status(500).send(new UserError(error));
+}
+
+function UserError(error) {
   this.status = 500;
   this.responseText = 'Sorry, something went wrong';
   this.error = error;
 }
 
 client.connect()
-  .then(()=>{
-    server.listen( PORT, () =>{
+  .then(() => {
+    server.listen(PORT, () => {
       console.log(`listen ${PORT}`);
     });
-  });
+  })
+  .catch(err => console.log(err));
 
 
